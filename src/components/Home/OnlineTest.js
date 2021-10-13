d4fdaa688e2590436db37463aeff8f0f4ade0865
Rustam21 <samsung122356@mail.ru>
2021-10-13 10:48:59 +0500

    't'

--- src/components/Home/OnlineTest.js
+++ src/components/Home/OnlineTest.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../../css/home/online_test.scss'
 import rectangle from '../../img/home/Rectangle 22.png'
-import backRectangle from '../../img/home/Rectangle 21.png'
 import { Link } from 'react-router-dom';
 
 export const OnlineTest = () => {
